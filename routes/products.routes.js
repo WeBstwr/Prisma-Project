3e0a689f884935d666bc59ddcad6f545bf567122
WeBstwr <ifedhawebster@gmail.com>
2024-06-27 19:01:37 +0300

    implement all the routes
    I have implemented all the routes.

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -16,6 +16,7 @@ router.post("/", async (req, res) => {
         totalPrice,
       },
       select: {
+        id: true,
         quantity: true,
         totalPrice: true,
       },
@@ -27,16 +28,80 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  res.send("getting a single product");
+router.get("/", async (req, res) => {
+  try {
+    const products = await prisma.order.findMany();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 });
 
-router.patch("/:id", (req, res) => {
-  res.send("updating a product");
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await prisma.order.findFirst({
+      where: { id },
+      select: {
+        id: true,
+        totalPrice: true,
+        quantity: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  res.send("deleting a product");
+router.patch("/:id", async (req, res) => {
+  const { orderId, quantity, totalPrice } = req.body;
+  const { id } = req.params;
+  try {
+    let updateProduct;
+    if (orderId) {
+      updateProduct = await prisma.order.update({
+        where: { id },
+        data: { orderId },
+      });
+    }
+    if (quantity) {
+      updateProduct = await prisma.order.update({
+        where: { id },
+        data: { quantity },
+      });
+    }
+    if (totalPrice) {
+      updateProduct = await prisma.order.update({
+        where: { id },
+        data: { totalPrice },
+      });
+    }
+    res.status(200).json(updateProduct);
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await prisma.order.delete({
+      where: { id },
+      select: {
+        id: true,
+        totalPrice: true,
+        quantity: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 });
 
 export default router;
